de3d75a7c6797f816be6482093f0cac31e6cb05e
Cauê Justen Garbi <126606467+garbizada@users.noreply.github.com>
2023-09-27 10:24:21 -0300

    Add files via upload

---
+++ Esporte_exercicio/src/esporte/Futebol.java
@@ -0,0 +1,13 @@
+package esporte;
+
+public class Futebol extends Esporte{
+	private int qtd_tempos;
+
+	public int getQtd_tempos() {
+		return qtd_tempos;
+	}
+
+	public void setQtd_tempos(int qtd_tempos) {
+		this.qtd_tempos = qtd_tempos;
+	}
+}
---
+++ Esporte_exercicio/src/esporte/natacao.java
@@ -0,0 +1,15 @@
+package esporte;
+
+public class natacao extends Esporte {
+	
+	private int distancia_prova;
+
+	public int getDistancia_prova() {
+		return distancia_prova;
+	}
+
+	public void setDistancia_prova(int distancia_prova) {
+		this.distancia_prova = distancia_prova;
+	}
+
+}
