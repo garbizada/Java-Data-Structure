ce2ecc6087abcd06462c59dfb4ae33d15bd1566b
Cauê Justen Garbi <126606467+garbizada@users.noreply.github.com>
2023-09-27 10:22:46 -0300

    Add files via upload

---
+++ Formula_1.java
@@ -0,0 +1,15 @@
+package esporte;
+
+public class Formula_1 extends Esporte {
+	
+	private String marca_carro;
+
+	public String getMarca_carro() {
+		return marca_carro;
+	}
+
+	public void setMarca_carro(String marca_carro) {
+		this.marca_carro = marca_carro;
+	}
+
+}
